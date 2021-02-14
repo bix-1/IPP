e19a2f779b7aae687ab4e0469f73bfc3b59c83d0
xbartk07 <xbartk07@stud.fit.vutbr.cz>
2021-02-14 15:57:31 +0100

    Fixes; Added string correction

--- src/parse.php
+++ src/parse.php
@@ -1,8 +1,4 @@
 <?php
-// TODO
-//  problematicke znaky XML <>$ ...
-//  type matching
-
 $output = "";
 
 abstract class State {
@@ -10,59 +6,37 @@ abstract class State {
   const Command = 1;
 }
 
-/*
-abstract class Type {
-  const INV = -1;
-  const Int     = 0;
-  const Bool    = 1;
-  const String  = 2;
-  const Nil     = 3;
-  const Label   = 4;
-  const Type    = 5;
-  const Var     = 6;
-}
-*/
-
-
 $opcodes = array(
   "MOVE" => array("var", "symb"),
   "CREATEFRAME" => array(),
   "PUSHFRAME" => array(),
   "POPFRAME" => array(),
   "DEFVAR" => array("var"),
-
   "CALL" => array("label"),
-  "RETURN" => array("symb"),
-  "PUSHS" => array("var"),
+  "RETURN" => array(),
+  "PUSHS" => array("symb"),
   "POPS" => array("var"),
-
   "ADD" => array("var", "symb", "symb"),
   "SUB" => array("var", "symb", "symb"),
   "MUL" => array("var", "symb", "symb"),
-  "IDIV" => array(),
-
+  "IDIV" => array("var", "symb", "symb"),
   "LT" => array("var", "symb", "symb"),
   "GT" => array("var", "symb", "symb"),
   "EQ" => array("var", "symb", "symb"),
-
-  "AND" => array("var", "symb_bool", "symb_bool"),
-  "OR" => array("var", "symb_bool", "symb_bool"),
-  "NOT" => array("var", "symb_bool", "symb_bool"),
-
+  "AND" => array("var", "symb", "symb"),
+  "OR" => array("var", "symb", "symb"),
+  "NOT" => array("var", "symb"),
   "INT2CHAR" => array("var", "symb"),
   "STRI2INT" => array("var", "symb", "symb"),
-
   "READ" => array("var", "type"),
   "WRITE" => array("symb"),
   "CONCAT" => array("var", "symb", "symb"),
   "STRLEN" => array("var", "symb"),
-
   "GETCHAR" => array("var", "symb", "symb"),
   "SETCHAR" => array("var", "symb", "symb"),
   "TYPE" => array("var", "symb"),
   "LABEL" => array("label"),
   "JUMP" => array("label"),
-
   "JUMPIFEQ" => array("label", "symb", "symb"),
   "JUMPIFNEQ" => array("label", "symb", "symb"),
   "EXIT" => array("symb"),
@@ -77,8 +51,16 @@ function parse() {
   $output .= "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
   $output .= "<program language=\"IPPcode21\">\n";
 
+  while (true) {
+    if (feof(STDIN)) {
+      if ($state == State::Start) {
+        fwrite(STDERR, "ERROR: Missing header in src file\n");
+        exit(21);
+      }
+      else return;
+    }
 
-  while (!feof(STDIN) && $line = fgets(STDIN)) {
+    $line = fgets(STDIN);
     // remove comments
     $line = preg_replace("/#.*/", "", $line);
     // skip empty line
@@ -87,8 +69,8 @@ function parse() {
     }
 
     // HEADER
-    if ($state == State::Start) {
-      if (preg_match("/^\s*\.IPPcode21\s*$/i", "$line")) {
+    if ($state == State::Start) { // TODO 21212121
+      if (preg_match("/^\s*\.IPPcode19\s*$/i", "$line")) {
         $state = State::Command;
         continue;
       }
@@ -100,18 +82,16 @@ function parse() {
     // COMMANDS
     else if ($state == State::Command) {
       // split into words
-      $instr = preg_split("/\s+/", $line);
+      $instr = preg_split("/\s+/", ltrim($line));
       // remove last _blank_ element
       array_pop($instr);
-
+      // parse instruction
       handle_instr($instr);
     }
   }
 
   $output .= "</program>\n";
-
-  echo "$output";
-  exit(0);
+  echo $output;
 }
 
 function handle_instr($instr) {
@@ -146,30 +126,27 @@ function handle_args($instr) {
   // match types of args
   for ($i = 0; $i < $n; $i++) {
     $arg = handle_type($instr[$i+1], $types[$i]);
-    if (!strcmp($arg[0], "INV")) {
-      fwrite(STDERR, "ERROR: Invalid type of arg [$i] for \"$instr[0]\"\n");
-      exit(23);
+    if (!strcmp($arg[0],"var") || !strcmp($arg[0],"string")) {
+      $arg[1] = correct_string($arg[1]);
     }
-    else {
-      $output .= "\t\t<arg".($i+1)." type=\"$arg[0]\">$arg[1]</arg".($i+1).">\n";
-    }
+    $output .= "\t\t<arg".($i+1)." type=\"$arg[0]\">$arg[1]</arg".($i+1).">\n";
   }
 }
 
 // checks whether type of given arg & defined type match
 // returns arg as array of [_type_, _value_] if types match
-// return ["INV"] if types do not match
+// else error
 function handle_type($arg, $type) {
   switch ($type) {
     case "var":
-      if (!preg_match("/^(GF|LF|TF)@[a-zA-Z_\-$&%*!?][a-zA-Z0-9_\-$&%*!?]*$/", $arg)) {
+      if (!preg_match("/^\s*(GF|LF|TF)@[a-zA-Z_\-$&%*!?][a-zA-Z0-9_\-$&%*!?]*$/", $arg)) {
         fwrite(STDERR, "ERROR: Invalid variable\n");
         exit(23);
       }
       return array("var", $arg);
 
     case "label":
-      if (!preg_match("/^[a-zA-Z_\-$&%*!?][a-zA-Z0-9_\-$&%*!?]*$/", $arg)) {
+      if (!preg_match("/^\s*[a-zA-Z_\-$&%*!?][a-zA-Z0-9_\-$&%*!?]*$/", $arg)) {
         fwrite(STDERR, "ERROR: Invalid label\n");
         exit(23);
       }
@@ -178,12 +155,13 @@ function handle_type($arg, $type) {
     case "symb":
     // check for var
     if (
-      preg_match("/^(GF|LF|TF)@[a-zA-Z_\-$&%*!?][a-zA-Z0-9_\-$&%*!?]*$/", $arg)
+      preg_match("/^\s*(GF|LF|TF)@[a-zA-Z_\-$&%*!?][a-zA-Z0-9_\-$&%*!?]*$/", $arg)
     ) {
       return array("var", $arg);
     }
+    // check for const
     else if (
-      preg_match("/^((int@([+-][0-9]+|[0-9]+))|(bool@(true|false))|(string@(|([^\s#\\\\]|\\\\([0-9][0-9][0-9]))+))|(nil@nil))$/", $arg)
+      preg_match("/^\s*((int@([+-][0-9]+|[0-9]+))|(bool@(true|false))|(string@(|([^\s#\\\\]|\\\\([0-9][0-9][0-9]))+))|(nil@nil))$/", $arg)
     ) {
       $tmp = preg_split("/@/", $arg, 2);
       return array($tmp[0], $tmp[1]);
@@ -193,20 +171,34 @@ function handle_type($arg, $type) {
       exit(23);
     }
 
-    case "type":  // TODO
+    case "type":
+      if (preg_match("/^\s*(int|string|bool)$/", $arg)) {
+        return array("type", $arg);
+      }
+      else {
+        fwrite(STDERR, "ERROR: Invalid type \"$arg\"\n");
+        exit(23);
+      }
 
     default:
+      return array("INV");
       break;
   }
-
-
 }
 
+function correct_string($str) {
+  str_replace("\"", "&quot;", $str);
+  str_replace("&", "&amp;", $str);
+  str_replace("'", "&apos;", $str);
+  str_replace("<", "&lt;", $str);
+  str_replace(">", "&gt;", $str);
+  return $str;
+}
 
 
 # OPTION HANDLING
 if ($argc > 2) {
-  fwrite(STDERR, "ERROR: Invalid arguments\n");
+  fwrite(STDERR, "ERROR: Invalid options\n");
   exit(10);
 }
 else if ($argc == 2) {
@@ -215,14 +207,12 @@ else if ($argc == 2) {
     exit(0);
   }
   else {
-    fwrite(STDERR, "ERROR: Invalid arguments\n");
+    fwrite(STDERR, "ERROR: Invalid options\n");
     exit(1);
   }
 }
 
-
 # PARSING
 parse();
 exit(0);
-
 ?>
