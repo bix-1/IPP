36cc0d4754867fc3dcf581d6a07c3f5e4cdd102b
xbartk07 <xbartk07@stud.fit.vutbr.cz>
2021-03-12 14:26:12 +0100

    Interpret init

---
+++ src/interpret.py
@@ -0,0 +1,33 @@
+# File: interpret.py
+# Brief: Implementation of interpret
+#
+# Project: Interpret for imperative programing language IPPcode21
+#
+# Authors: Jakub Bartko    xbartk07@stud.fit.vutbr.cz
+
+
+# TODO:
+# arg parsing
+# reading source & input
+
+
+import sys
+import argparse
+
+# parser = argparse.ArgumentParser(description="Interpret XML input.")
+# parser.add_argument("sourcefile" help="an integer for the accumulator")
+#
+# args = parser.parse_args()
+
+'''
+import sys
+if file_name_given:
+    inf = open(file_name_given)
+else:
+    inf = sys.stdin
+'''
+
+'''
+if inf is not sys.stdin:
+    inf.close()
+'''
