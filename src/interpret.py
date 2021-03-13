a5482bcce937267d96ca213459c9dc83f32359dc
xbartk07 <xbartk07@stud.fit.vutbr.cz>
2021-03-13 15:54:49 +0100

    Added commandline arg parsing

--- src/interpret.py
+++ src/interpret.py
@@ -13,21 +13,55 @@
 
 import sys
 import argparse
+import xml.etree.ElementTree as ET
 
-# parser = argparse.ArgumentParser(description="Interpret XML input.")
-# parser.add_argument("sourcefile" help="an integer for the accumulator")
-#
-# args = parser.parse_args()
 
-'''
-import sys
-if file_name_given:
-    inf = open(file_name_given)
+def error(msg, code):
+    print("ERROR: " + msg, file=sys.stderr)
+    sys.exit(code)
+
+
+
+
+# handle command line arguments
+aparser = argparse.ArgumentParser(description="Interpret XML input.")
+aparser.add_argument(
+    "--source",
+    required = False,
+    default = "",
+    help="input file with XML representation of IPPcode21")
+aparser.add_argument(
+    "--input",
+    required = False,
+    default = "",
+    help="input file with XML representation of IPPcode21")
+
+args = aparser.parse_args()
+
+if not (args.source or args.input):
+    error("At least one of --source, --input needs to be specified", 11)
+
+# get source stream
+if args.source:
+    src = open(args.source, "r")
 else:
-    inf = sys.stdin
-'''
+    src = sys.stdin
+# get input stream
+if args.input:
+    input = open(args.input, "r")
+else:
+    input = sys.stdin
 
-'''
-if inf is not sys.stdin:
-    inf.close()
-'''
+
+tree = ET.parse(src)
+root = tree.getroot()
+for child in root.iter():
+    print(child.tag, child.attrib)
+
+
+
+# teardown
+if src is not sys.stdin:
+    src.close()
+if input is not sys.stdin:
+    input.close()
