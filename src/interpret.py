5f040bdfc19893b1c16f7c1f05a8086b3181af47
xbartk07 <xbartk07@stud.fit.vutbr.cz>
2021-03-28 16:07:51 +0200

    Added validation of instr order

--- src/interpret.py
+++ src/interpret.py
@@ -51,6 +51,13 @@ def get_args():
     return args.source, args.input
 
 
+def iterate_instructions(root):
+    for child in root:
+        print(child.tag, child.attrib)
+        for attr in child:
+            print("\t", attr.attrib["type"], ": ", attr.text)
+
+
 def main():
     src, input = get_args()
 
@@ -69,11 +76,16 @@ def main():
         error("Invalid root element", 32)
 
     # sort instructions by [attribute] order
-    root[:] = sorted(root, key=lambda x: x.attrib["order"])
+    try:
+        root[:] = sorted(root, key=lambda x: int(x.attrib["order"]))
+        if root[0].attrib["order"] != "1":
+            raise ValueError
+    except KeyError:
+        error("Instruction missing attribute \"order\"", 32)
+    except ValueError:
+        error("Instruction's attribute \"order\" has invalid value", 32)
 
-    for child in root.iter():
-        print(child.tag, child.attrib)
-
+    iterate_instructions(root)
 
 if __name__ == "__main__":
     main()
